ad977ca430f97b1ae245c282f7d9644cff79f73a
Tamir Levital <tamir.levital@gmail.com>
2019-06-20 12:04:58 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// import bookHeader from './cmps/header.app.cmp.js'
+import bookHeader from './cmps/header.app.cmp.js'
 import routes from './routes.js';
 
 const router = new VueRouter({ routes })
@@ -7,7 +7,7 @@ var app = new Vue({
     el: '#app',
     template: `
     <div>
-       <!-- <book-header></book-header> -->
+       <book-header></book-header>
        <!-- <router-view></router-view> -->
        <!-- <footer>Tshompyrights 2019</footer> -->
     </div>
