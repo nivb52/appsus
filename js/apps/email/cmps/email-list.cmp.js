e2c6f263142b4670a88ba028d756cc042d0c3ed2
Niv Bekelman <nivb52@gmail.com>
2019-06-21 19:28:23 +0300

    just notation in the html part

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -4,17 +4,17 @@ export default {
     name: 'email-list',
     template: `
 <section>
-    
 
     <div id="main-nano-wrapper" class="nano">
       <div class="nano-content">
+          
         <ul class="message-list">
-            
-                <email-preview v-for="email in emails" :key="email.id" :email="email" ></email-preview>
-
+          <email-preview v-for="email in emails" :key="email.id" :email="email" ></email-preview>
         </ul>
+
      </div>
     </div>
+
 </section>
 
     `,
